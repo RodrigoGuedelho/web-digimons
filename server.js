ab2456493b34883892913c4711068952ca77a6c5
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2021-02-28 22:15:16 -0300

    Ajustes para deploy.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/my-app"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(__dirname + "/dist/index.html");
+  res.sendFile(__dirname + "/dist/my-app/index.html");
 
 });
 
